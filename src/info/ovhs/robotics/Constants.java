b53c3c07e8feb883fdf8601d3201e835316f0101
SnowyKyubii00 <eric.truong3@gmail.com>
2015-01-12 19:30:05 -0800

    Added tote and bot weight

--- src/info/ovhs/robotics/Constants.java
+++ src/info/ovhs/robotics/Constants.java
@@ -5,4 +5,8 @@ public final class Constants {
 	public final int MAX_JOYSTICK_AXIS = 1;
 	
 	public final int MIN_JOYSTICK_AXIS = -1;
+	
+	public final int TOTE_WEIGHT = 6;
+	
+	public final int ROBOT_WEIGHT = 120;
 }
