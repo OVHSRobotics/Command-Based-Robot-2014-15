f78aea91670185144bac538e927af88b743d0c4e
cjbaskin123 <cjbaskin123@gmail.com>
2015-01-14 14:55:00 -0800

    Renamed NoodleLiftingConveyerBelt to ConveyerBelt to correct for naming discrepancy

--- src/info/ovhs/robotics/subsystems/ConveyerBelt.java
+++ src/info/ovhs/robotics/subsystems/ConveyerBelt.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class NoodleLiftingConveyerBelt extends Subsystem {
+public class ConveyerBelt extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
