f16f5f486becd12bb730db9a21c6fa79bece7aee
kvo-robopsych <sonyvo1@gmail.com>
2015-01-23 19:16:09 -0800

    Added motor on and off commands and a "requires" function

--- src/info/ovhs/robotics/commands/DropTrashCan.java
+++ src/info/ovhs/robotics/commands/DropTrashCan.java
@@ -1,5 +1,6 @@
 package info.ovhs.robotics.commands;
 
+import info.ovhs.robotics.Constants;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,27 +11,36 @@ public class DropTrashCan extends Command {
     public DropTrashCan() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(CommandBase.conveyerBelt);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	double percentSpeed = 100;
+    	CommandBase.conveyerBelt.backward(percentSpeed / 100 * -Constants.MOTOR_MAX_OUTPUT);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	// Do nothing
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
+    	// Command is done when encoder reaches certain amount of ticks
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	// Turn motor off
+    	CommandBase.conveyerBelt.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	// Turn motor off
+    	CommandBase.conveyerBelt.stop();	
     }
 }
