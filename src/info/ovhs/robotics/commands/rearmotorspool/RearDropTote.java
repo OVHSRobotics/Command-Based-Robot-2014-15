b809607e271fbb52b248a7411711214d19437e70
kvo-robopsych <sonyvo1@gmail.com>
2015-01-29 18:58:22 -0800

    Created rear motor spool commands package using copy of conveyer belt commands
    Renamed each file with prefix "Rear"

---
+++ src/info/ovhs/robotics/commands/rearmotorspool/RearDropTote.java
@@ -0,0 +1,48 @@
+package info.ovhs.robotics.commands.rearmotorspool;
+
+import info.ovhs.robotics.Constants;
+import info.ovhs.robotics.commands.CommandBase;
+
+
+public class RearDropTote extends CommandBase {
+
+	public double initialDistance;
+	
+    public RearDropTote() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(CommandBase.conveyerBelt);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	this.initialDistance = CommandBase.conveyerBelt.encoder.getDistance();
+    	
+    	// Turn the motor on
+    	double percentSpeed = 100;
+    	CommandBase.conveyerBelt.backward(percentSpeed / 100 * -Constants.MotorConstants.MOTOR_MAX_OUTPUT);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	// Encoder reaches threshold
+    	return CommandBase.conveyerBelt.encoder.getDistance() <= this.initialDistance - Constants.TOTE_HEIGHT_INCHES;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	// Turn motor off
+    	CommandBase.conveyerBelt.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	// Turn motor off
+    	CommandBase.conveyerBelt.stop();
+    }
+}
---
+++ src/info/ovhs/robotics/commands/rearmotorspool/RearDropTrashCan.java
@@ -0,0 +1,47 @@
+package info.ovhs.robotics.commands.rearmotorspool;
+
+import info.ovhs.robotics.Constants;
+import info.ovhs.robotics.commands.CommandBase;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RearDropTrashCan extends Command {
+
+    public RearDropTrashCan() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(CommandBase.conveyerBelt);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	double percentSpeed = 100;
+    	CommandBase.conveyerBelt.backward(percentSpeed / 100 * -Constants.MotorConstants.MOTOR_MAX_OUTPUT);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	// Do nothing
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	// Command is done when encoder reaches certain amount of ticks
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	// Turn motor off
+    	CommandBase.conveyerBelt.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	// Turn motor off
+    	CommandBase.conveyerBelt.stop();	
+    }
+}
