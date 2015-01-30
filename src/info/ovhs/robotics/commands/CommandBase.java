1d6094e8e1141440a663252ceefdb0a9db6c87e3
OVHSRobotics4619 <ovhsrobotics@gmail.com>
2015-01-29 19:53:11 -0800

    Added Rear Motor Spool to Commandbase

--- src/info/ovhs/robotics/commands/CommandBase.java
+++ src/info/ovhs/robotics/commands/CommandBase.java
@@ -3,6 +3,7 @@ package info.ovhs.robotics.commands;
 import info.ovhs.robotics.OI;
 import info.ovhs.robotics.subsystems.ConveyerBelt;
 import info.ovhs.robotics.subsystems.DriveTrain;
+import info.ovhs.robotics.subsystems.RearMotorSpool;
 import edu.wpi.first.wpilibj.command.Command;
 
 public abstract class CommandBase extends Command {
@@ -10,10 +11,12 @@ public abstract class CommandBase extends Command {
 	public static DriveTrain driveTrain;
 	public static OI oi;
 	public static ConveyerBelt conveyerBelt;
+	public static RearMotorSpool rearMotor;
 	
 	public static void init() {
 		CommandBase.driveTrain = DriveTrain.getInstance();
 		CommandBase.oi = OI.getInstance();
 		CommandBase.conveyerBelt = ConveyerBelt.getInstance();
+		CommandBase.rearMotor = RearMotorSpool.getInstance();
 	}
 }
