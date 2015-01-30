ae16bef27fae900b02db8aefb6206b8b86af719a
kvo-robopsych <sonyvo1@gmail.com>
2015-01-29 20:02:31 -0800

    Replaced "conveyerBelt" with "rearMotor" within RearDropTote and RearLiftTote

--- src/info/ovhs/robotics/commands/rearmotorspool/RearDropTote.java
+++ src/info/ovhs/robotics/commands/rearmotorspool/RearDropTote.java
@@ -11,16 +11,16 @@ public class RearDropTote extends CommandBase {
     public RearDropTote() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(CommandBase.conveyerBelt);
+    	requires(CommandBase.rearMotor);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	this.initialDistance = CommandBase.conveyerBelt.encoder.getDistance();
+    	this.initialDistance = CommandBase.rearMotor.encoder.getDistance();
     	
     	// Turn the motor on
     	double percentSpeed = 100;
-    	CommandBase.conveyerBelt.backward(percentSpeed / 100 * -Constants.MotorConstants.MOTOR_MAX_OUTPUT);
+    	CommandBase.rearMotor.backward(percentSpeed / 100 * -Constants.MotorConstants.MOTOR_MAX_OUTPUT);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -30,19 +30,19 @@ public class RearDropTote extends CommandBase {
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
     	// Encoder reaches threshold
-    	return CommandBase.conveyerBelt.encoder.getDistance() <= this.initialDistance - Constants.TOTE_HEIGHT_INCHES;
+    	return CommandBase.rearMotor.encoder.getDistance() <= this.initialDistance - Constants.TOTE_HEIGHT_INCHES;
     }
 
     // Called once after isFinished returns true
     protected void end() {
     	// Turn motor off
-    	CommandBase.conveyerBelt.stop();
+    	CommandBase.rearMotor.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
     	// Turn motor off
-    	CommandBase.conveyerBelt.stop();
+    	CommandBase.rearMotor.stop();
     }
 }
--- src/info/ovhs/robotics/commands/rearmotorspool/RearLiftTote.java
+++ src/info/ovhs/robotics/commands/rearmotorspool/RearLiftTote.java
@@ -12,14 +12,14 @@ public class RearLiftTote extends Command {
     public RearLiftTote() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(CommandBase.conveyerBelt);
+    	requires(CommandBase.rearMotor);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	// Turn on motor
     	double percentSpeed = 100;
-    	CommandBase.conveyerBelt.forward(percentSpeed / 100 * Constants.MotorConstants.MOTOR_MAX_OUTPUT);
+    	CommandBase.rearMotor.forward(percentSpeed / 100 * Constants.MotorConstants.MOTOR_MAX_OUTPUT);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -36,13 +36,13 @@ public class RearLiftTote extends Command {
     // Called once after isFinished returns true
     protected void end() {
     	// Turn motor off
-    	CommandBase.conveyerBelt.stop();
+    	CommandBase.rearMotor.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
     	// Turn motor off
-    	CommandBase.conveyerBelt.stop();
+    	CommandBase.rearMotor.stop();
     }
 }
