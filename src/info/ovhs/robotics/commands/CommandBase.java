3962f444f68cc869630bc51d0a0c8ea561101520
cjbaskin123 <cjbaskin123@gmail.com>
2015-01-31 18:58:56 -0800

    Attempted to add PID DriveTrain Subsystem

--- src/info/ovhs/robotics/commands/CommandBase.java
+++ src/info/ovhs/robotics/commands/CommandBase.java
@@ -3,6 +3,7 @@ package info.ovhs.robotics.commands;
 import info.ovhs.robotics.OI;
 import info.ovhs.robotics.subsystems.ConveyerBelt;
 import info.ovhs.robotics.subsystems.DriveTrain;
+import info.ovhs.robotics.subsystems.PIDDriveTrain;
 import info.ovhs.robotics.subsystems.RearMotorSpool;
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -12,11 +13,13 @@ public abstract class CommandBase extends Command {
 	public static OI oi;
 	public static ConveyerBelt conveyerBelt;
 	public static RearMotorSpool rearMotorSpool;
+	public static PIDDriveTrain pidDriveTrain;
 	
 	public static void init() {
 		CommandBase.driveTrain = DriveTrain.getInstance();
 		CommandBase.conveyerBelt = ConveyerBelt.getInstance();
 		CommandBase.rearMotorSpool = RearMotorSpool.getInstance();
+		CommandBase.pidDriveTrain = PIDDriveTrain.getInstance();
 		
 		// Must be initialized after all subsystems
 		CommandBase.oi = OI.getInstance();
--- src/info/ovhs/robotics/subsystems/PIDDriveTrain.java
+++ src/info/ovhs/robotics/subsystems/PIDDriveTrain.java
@@ -1,5 +1,8 @@
 package info.ovhs.robotics.subsystems;
 
+import info.ovhs.robotics.OI;
+import info.ovhs.robotics.RobotMap;
+import info.ovhs.robotics.commands.CommandBase;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /**
@@ -7,8 +10,14 @@ import edu.wpi.first.wpilibj.command.PIDSubsystem;
  */
 public class PIDDriveTrain extends PIDSubsystem {
 
+	private static final double k_p = 1;
+	private static final double k_i = 0.1;
+	private static final double k_d = 0;
+	private static PIDDriveTrain instance;
+	
     // Initialize your subsystem here
     public PIDDriveTrain() {
+    	super(k_p, k_i, k_d);
     	
         // Use these to get going:
         // setSetpoint() -  Sets where the PID controller should move the system
@@ -17,6 +26,7 @@ public class PIDDriveTrain extends PIDSubsystem {
     }
     
     public void initDefaultCommand() {
+    	
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
@@ -25,11 +35,29 @@ public class PIDDriveTrain extends PIDSubsystem {
         // Return your input value for the PID loop
         // e.g. a sensor, like a potentiometer:
         // yourPot.getAverageVoltage() / kYourMaxVoltage;
-    	return 0.0;
+    	return RobotMap.gyro1.getAngle();
     }
     
     protected void usePIDOutput(double output) {
+    	if (OI.getRightStickXAxis() == 0) {
+    		RobotMap.drive.mecanumDrive_Cartesian(OI.getLeftStickXAxis(), OI.getLeftStickYAxis(), 0, RobotMap.gyro1.getAngle());
+    	} else {
+    		RobotMap.drive.mecanumDrive_Cartesian(OI.getLeftStickXAxis(), OI.getLeftStickYAxis(), output, RobotMap.gyro1.getAngle());
+    	}
         // Use output to drive your system, like a motor
         // e.g. yourMotor.set(output);
     }
+    
+    /**
+	 * Gets instance of PIDDriveTrain Subsystem or creates one if one is not present
+	 * 
+	 * @return Instance of PIDDriveTrain Subsystem
+	 */
+	public static PIDDriveTrain getInstance() {
+		if (PIDDriveTrain.instance == null) {
+			PIDDriveTrain.instance = new PIDDriveTrain();
+		}
+		
+		return PIDDriveTrain.instance;
+	}
 }
