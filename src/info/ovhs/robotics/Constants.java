c177650d7a533ea590000cc889c678b2f6f832cc
cjbaskin123 <cjbaskin123@gmail.com>
2015-03-09 14:58:57 -0700

    Adjusted Deadzones for operator controllers

--- src/info/ovhs/robotics/Constants.java
+++ src/info/ovhs/robotics/Constants.java
@@ -158,9 +158,9 @@ public final class Constants {
 					
 					public static final double Z_CENTER = 0;
 					
-					public static final double X = 0.10;
+					public static final double X = 0.19;
 					
-					public static final double Y = 0.07;
+					public static final double Y = 0.12;
 					
 					public static final double Z = 0.00;
 				}
@@ -218,9 +218,9 @@ public final class Constants {
 					
 					public static final double Z_CENTER = 0;
 					
-					public static final double X = 0.10;
+					public static final double X = 0.19;
 					
-					public static final double Y = 0.07;
+					public static final double Y = 0.12;
 					
 					public static final double Z = 0.00;
 				}
@@ -381,6 +381,7 @@ public final class Constants {
 		}	
 	}
 	public static final class ActiveInput {
+		
 		public static final double AUTO_MOVE_SPEED = .75;
 	}
 }
