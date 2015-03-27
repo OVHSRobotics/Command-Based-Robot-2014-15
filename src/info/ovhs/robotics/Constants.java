5d49d00b83502c2dd08c43b719921adf48baf5b7
cjbaskin123 <cjbaskin123@gmail.com>
2015-03-27 14:03:40 -0700

    changed spin can speed

--- src/info/ovhs/robotics/Constants.java
+++ src/info/ovhs/robotics/Constants.java
@@ -391,6 +391,6 @@ public final class Constants {
 		
 		public static final double AUTO_MOVE_SPEED = .50;
 		
-		public static final double SPIN_MOVE_SPEED = .15;
+		public static final double SPIN_MOVE_SPEED = .35;
 	}
 }
